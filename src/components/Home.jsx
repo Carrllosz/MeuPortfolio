cdbe15ec02cfdb2aafa356cd5959c4b95da259fc
joaocarlos <joaocar2003@gmail.com>
2024-02-18 22:16:19 -0300

    alterações no texto Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className='max-w-[1000px] mx-auto px-6 flex flex-col justify-center h-full'>
         <p className='text-2xl sm:text-3xl font-bold text-[#E45338]'>Olá, meu nome é</p>
         <h1 className='text-[90px] sm:text-8xl font-bold text-[#fff] text-center'> João Carlos</h1>
-        <div className='flex w-full text-1xl sm:text-7x1 font-bold text-[#E45338] border-2 px-4 py-3 my-2 border-[#E45338] rounded-full'>
+        <div className='flex w-full text-center justify-center text-1xl sm:text-7x1 font-bold text-[#E45338] border-2 px-4 py-3 my-2 border-[#E45338] rounded-full'>
           <h2>Desenvolvedor Front-end e Mobile</h2>
         </div>
       </div>
